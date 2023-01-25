652f2206b98ddf726b08c9eba00d1bd8060859d9
gumquat <gumquat@gmail.com>
2023-01-25 09:50:16 -0800

    :P

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+*main - prints a sentence using puts command
+*Return: main returns 0
+*/
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+char name[] = "with proper grammar, but the outcome is a piece of art,";
+printf(name);
+return (0);
+}
