cbca9636d9167c040a656a1c13a1113a960b1aaa
gumquat <gumquat@gmail.com>
2023-01-25 09:33:30 -0800

    :P

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/* 
+ * main - prints a sentence using puts command
+ * Return: return 0
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char name[] = "\"Programming is like building a multilingual puzzle";
+	       puts (name);
+	       return (0);
+}
