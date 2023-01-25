b14f3bf9963b9a1707a1533a7c8e37e5a1b84a43
gumquat <gumquat@gmail.com>
2023-01-25 09:37:13 -0800

    :P

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	char name[]="\"Programming is like building a multilingual puzzle";
+	char name[] = "\"Programming is like building a multilingual puzzle";
 	       puts(name);
-	       return(0);
+	       return (0);
 }
