49580976674e94ce435420273710556ef2de5429
gumquat <gumquat@gmail.com>
2023-01-25 12:59:59 -0800

    :P

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,17 +11,17 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n>0)
+if (n > 0)
 {
 printf("%d is positive\n");
 }
 else if
 {
-printf("%d is negative\n");	
+printf("%d is negative\n");
 }
 else
 {
-printf("%d is zero\n");	
+printf("%d is zero\n");
 }
 return (0);
 }
