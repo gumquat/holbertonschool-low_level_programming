dfc0abe5617d63114cdb0dedddbd73446583d544
gumquat <gumquat@gmail.com>
2023-01-26 11:07:16 -0800

    :P

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+/**
+* main - PUT DESCRIPTION HERE
+* Return: main always 0
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+char i;
+char pp;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+for (pp = 'A'; pp <= 'Z'; pp++)
+{
+putchar(pp);	
+}
+putchar('\n');
+return (0);
+}
