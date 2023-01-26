845c8ea68cfc59e8169c0ac23846a070e0fe7d19
gumquat <gumquat@gmail.com>
2023-01-26 12:00:56 -0800

    :P

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+/**
+*main - PUT DESCRIPTION HERE
+*Return: main always 0
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+int i;
+for 
+(i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
