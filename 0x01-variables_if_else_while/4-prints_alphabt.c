4e9f4670d5731667e28e9b90cebe3420d48a9d40
gumquat <gumquat@gmail.com>
2023-01-26 11:29:47 -0800

    :P

---
+++ 0x01-variables_if_else_while/4-prints_alphabt.c
@@ -0,0 +1,23 @@
+/**
+*main - WORDS
+*Return 0 always
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+	if (i == 'e' || i =='q')
+	{
+	}
+	else
+	{
+	putchar(i);
+	}
+}
+putchar('\n');
+return (0);
+}
