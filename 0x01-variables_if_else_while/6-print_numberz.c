2618c261500473a27ac1cbe26b0cc2cda9f46591
gumquat <gumquat@gmail.com>
2023-01-26 12:10:37 -0800

    :P

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+/**
+*main - PUT DESCRIPTION HERE
+*Return: main always 0
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar('0' + i);
+}
+putchar('\n');
+return (0);
+}
