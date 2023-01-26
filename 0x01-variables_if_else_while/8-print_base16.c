e388f54d7f7ec7eb68db13765b7633649fd43d7a
gumquat <gumquat@gmail.com>
2023-01-26 12:19:28 -0800

    :P

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+/**
+*main - PUT DESCRIPTION HERE
+*Return: main always 0
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+int i;
+char p;
+for (i = 0; i <= 16; i++)
+{
+putchar(i);
+}
+for (p = 'a'; p <= 'f'; p++)
+{
+putchar(p);
+}
+putchar('\n');
+return (0);
+}
