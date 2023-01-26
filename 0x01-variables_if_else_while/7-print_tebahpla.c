2411f02bc5db1653f7a4a3be29a22e73ac7ea79e
gumquat <gumquat@gmail.com>
2023-01-26 12:14:46 -0800

    :P

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+/**
+*main - PUT DESCRIPTION HERE
+*Return: main always 0
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+char i;
+for (i = 'z'; i >= 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
