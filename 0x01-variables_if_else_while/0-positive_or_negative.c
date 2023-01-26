81039203fe95959ee5b8bc41a5e425b43394d752
gumquat <gumquat@gmail.com>
2023-01-26 07:44:47 -0800

    :P

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,21 +2,21 @@
 *main - prints the size of various types
 *Return: main always 0
 */
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <iostream>
 /* more headers goes there */
 /*better style doc for function main goes there */
 int main(void)
 {
 int n;
 srand(time(0));
-n = rand() - RAND_MAX / 2;
+n == rand() - RAND_MAX / 2;
 if (n > 0)
 {
 printf("%d is positive\n");
 }
-else if
+else if(n==0);
 {
 printf("%d is negative\n");
 }
