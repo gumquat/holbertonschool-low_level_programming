66ee2fc0db437323ec99d273d2260f188c27fd1b
gumquat <gumquat@gmail.com>
2023-01-26 09:51:57 -0800

    :P

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+/**
+*main - PUT DESCRIPTION HERE
+*Return: main always 0
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+printf("%i ", i);
+}
+return (0);
+}
