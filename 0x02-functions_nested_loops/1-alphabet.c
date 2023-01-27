200b4326ff01cfbd3f9b986c50d66bdd0e6009bf
gumquat <gumquat@gmail.com>
2023-01-27 11:43:46 -0800

    :P

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,13 +3,9 @@
 *Return: void
 */
 #include "main.h"
-void print_alphabet(void)
-{
-char letter = 'a';
-while (letter <= 'z');
-{
-_putchar(letter);
-letter++;
+void main(void)
+{	
+print_alphabet();
 }
 _putchar('\n');
 return (0);
