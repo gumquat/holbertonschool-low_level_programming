9d9a8b730d9a1e0d4ff7b43af8b225c9eb7af40f
gumquat <gumquat@gmail.com>
2023-01-27 11:05:49 -0800

    :P

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,10 @@
 
 int main(void)
 {
-char sh = "_putchar";
-
-while (sh)
-{
-_putchar(sh);
-sh++;
-}
+char *myput = "_putchar";
+int i;
+for (i=0; myput[i]; i++)
+	_putchar(myput[i]);
 _putchar('\n');
 return (0);
 }
