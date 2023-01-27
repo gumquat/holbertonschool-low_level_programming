942e76f66cdb4cd7991d0be2151036ab67bd433e
gumquat <gumquat@gmail.com>
2023-01-27 08:46:53 -0800

    :P

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,12 @@
 *main - PUT DESCRIPTION HERE
 *Return: main always 0
 */
-
 #include "main.h"
-
 int name(void)
 {
-putchar('_putchar\n');
+char p;
+p = '_putchar';
+putchar(p);
+putchar('\n');
 return (0);
 }
