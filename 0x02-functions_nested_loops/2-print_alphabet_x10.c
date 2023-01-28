51f0dd42de9821f71803ddc037a57196a479c226
gumquat <gumquat@gmail.com>
2023-01-28 08:32:39 -0800

    :P

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+/**
+*main - PUT DESCRIPTION HERE
+*Return: main always 0
+*/
+
+#include "main.h"
+
+int main(void)
+{
+int i;
+char letter 'a';
+
+while (i < 10)
+{
+letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
+i++;
+return (0);
+}
