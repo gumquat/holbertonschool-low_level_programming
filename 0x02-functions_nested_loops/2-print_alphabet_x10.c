6e1dc655ef4ea2f9ef4796b70323e91960a92ba0
gumquat <gumquat@gmail.com>
2023-01-28 09:12:33 -0800

    :P

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- *  * print_alphabet_x10 - print alphabet 10 times
- *   *
- *    * Return: void
- *     */
+print_alphabet_x10 - print alphabet 10 times
+*
+* Return: void
+*/
 
 void print_alphabet_x10(void)
 {
