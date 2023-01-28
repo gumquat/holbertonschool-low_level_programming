c39f558059cda0aea5ee7a37db32c00a82ef6a27
gumquat <gumquat@gmail.com>
2023-01-28 12:12:51 -0800

    :P

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+/**
+*print_last_digit - print the last digit of an integer
+*@i: is a variable
+*Return: something lol
+*/
+
+#include "main.h"
+
+int print_last_digit(int i)
+{
+	int LastDigit = i % 10;
+	if (i < 0)
+	{
+		LastDigit = LastDigit * -1;
+	}
+	_putchar(LastDigit + '0');
+	return (LastDigit);
+}
