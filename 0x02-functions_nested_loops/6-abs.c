bef8aae14224e0d3fabc9096bd62fe88f9e4983d
gumquat <gumquat@gmail.com>
2023-01-28 11:49:45 -0800

    :P

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+/**
+*_abs - computes absolute value of an integer
+*@i: is a variable
+*Return: something lol
+*/
+
+#include "main.h"
+
+int int_abs(int i)
+{
+	if (i > 0)
+	{
+		return (n);
+	}
+	else if (i < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (i);
+	}
+}
