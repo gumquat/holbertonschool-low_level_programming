4567c6b6b6121e208dd4069b641b30f9957d7606
gumquat <gumquat@gmail.com>
2023-01-28 13:35:18 -0800

    :P

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,5 @@
 /**
-*Jack_Bauer - makes a whole ass clock
+*main - makes a whole ass clock
 *@a: hours of ten digits
 *@b: hours of single digits
 *@c: minutes of ten digits
@@ -9,7 +9,7 @@
 
 #include "main.h"
 
-int Jack_Bauer(void)
+int main(void)
 {
 
 int a, b, c, d;
