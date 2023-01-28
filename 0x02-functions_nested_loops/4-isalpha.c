b808e9dbf85832d64a33022234b521c0b63188bf
gumquat <gumquat@gmail.com>
2023-01-28 09:50:01 -0800

    :P

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+/**
+*int_isalpha - description HERE
+*@i: is a thing
+*Return: true if c is letter
+*/
+#include "main.h"
+int functionNAME(int i)
+{
+char letter = 'c';
+if (i >= 'a' && i <= 'z')
+{
+return (1);
+}
+{
+else if (i >= 'A' && <= 'Z')
+}
+else
+{
+return (0);
+}
+}
