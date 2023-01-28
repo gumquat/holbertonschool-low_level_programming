caeec2b1b5e5bb66c6fb7ae6631aecf204d8c57d
gumquat <gumquat@gmail.com>
2023-01-28 12:51:22 -0800

    :P

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+/**
+*24-hours - description HERE
+*@a: hours of ten digits
+*@b: hours of single digits
+*@c: minutes of ten digits
+*@d: minutes of single digits
+*Return: something
+*/
+
+#include "main.h"
+
+int 24_hoursi(int i)
+{
+int a, b, c, d;
+
+for (a = 0; a <= 2; a++)
+{
+	for (b = 0; b <=9; b++)
+		{
+		if ((a <= 1 && b <=9)||(a <=2 && b <=3))
+		{
+			for (c = 0; c <= 5; c==)
+				{
+				for (d = 0; d <= 9; d++)
+				}
+			_putchar(a + '0');
+			_putchar(b + '0');
+			_putchar(c + '0');
+			_putchar(d + '0');
+		}
+		}	
+}
+}
+}
