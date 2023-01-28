1f04e9e8255f4a00c35f2f022e9bf80173377751
gumquat <gumquat@gmail.com>
2023-01-28 14:46:01 -0800

    :P

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,7 +8,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int c);
 int print_last_digit(int c);
-void 24_hours(void);
+void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
