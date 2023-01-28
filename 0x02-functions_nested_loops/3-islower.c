1a145ee97e2d174f8be179ab5cc70076905e7472
gumquat <gumquat@gmail.com>
2023-01-28 09:24:29 -0800

    :P

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+/**
+*_islower - checks for lowercase
+*Return: 1 if lowecase
+*/
+
+#include "main.h"
+
+int main(int i)
+{
+if (i >= 'a' && i <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
