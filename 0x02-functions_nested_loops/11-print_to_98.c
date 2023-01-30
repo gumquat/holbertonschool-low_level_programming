41f356d6a23e956d7410e8a8d08bbacc9462704f
gumquat <gumquat@gmail.com>
2023-01-30 09:00:13 -0800

    :P

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+/**
+*functionnameHERE - description HERE
+*Return: something
+*/
+
+#include "main.h"
+
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (; n <= 98; n++)
+{
+if (n == 98)
+{	
+_putchar(n + '0');
+_putchar('\n');
+else
+{
+_putchar(n + " ,");
+}
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+_putchar(n + '0');
+_putchar('\n');
+}
+else
+{
+
+_putchar(n + " ,");
+}
+}
+}
+}
+}
+return (0);
+}
