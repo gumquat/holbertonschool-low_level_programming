1c34f175c2730b9b06607b5d80f1c55d585d3f06
gumquat <gumquat@gmail.com>
2023-02-02 10:19:47 -0800

    :P

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+/**
+*_isdigit - checks for digit
+@c: c is uhhhhhh
+*Return: 1 if digit
+*/
+#include "main.h"
+int _isdigit(int c)
+{
+	if (i >= 0 && i <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
