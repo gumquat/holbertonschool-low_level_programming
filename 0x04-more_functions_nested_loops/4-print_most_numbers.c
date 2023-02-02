f2f357e620e8f7dd521dd19c3a750d1295cf70ae
gumquat <gumquat@gmail.com>
2023-02-02 11:26:16 -0800

    :P

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+/**
+*print_numbers - PUT DESCRIPTION HERE
+*Return: main always 0
+*/
+
+#include "main.h"
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	if (i = 2 || i = 4)
+		{}
+		else
+		{
+		_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
