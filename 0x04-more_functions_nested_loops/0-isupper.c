4535c2698c99c6d12356d0c6c3ab53e588da54aa
gumquat <gumquat@gmail.com>
2023-02-02 10:08:27 -0800

    :P

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+/**
+*isupper - checks if upper case 
+*@variableNAME: variable description HERE
+*Return: 0 or 1 or other data HERE
+*/
+
+#include "main.h"
+
+int _isupper(int c)
+{
+
+	if (c >= 'A'&& i <= 'Z')
+		return (1);
+
+return (i);
+}
