a9c380e085e48ff84ef53540477a39e174b2bad0
gumquat <gumquat@gmail.com>
2023-02-02 10:47:08 -0800

    :P

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+*main - PUT DESCRIPTION HERE
+*Return: main always 0
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar(i + '0');
+	return (0);
+}
