33850b029a23c32f93fe5d53a27da2a9068e7315
gumquat <gumquat@gmail.com>
2023-02-02 13:29:14 -0800

    :P

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,15 @@
 void more_numbers(void)
 {
 
-int i = 0;
-int n = 0;
-	while (n < 10)
+int i;
+int n;
+
+	for (n = 0; n < 10 ; n++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
 			_putchar(i + '0');
 		}
 		_putchar('\n');
-		n++;
 	}
 }
