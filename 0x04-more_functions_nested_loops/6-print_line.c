8b51a7e6b66aaf16e0252de53d8ffd879cc514cb
gumquat <gumquat@gmail.com>
2023-02-03 09:15:16 -0800

    :P

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,26 +1,25 @@
 /**
 *print_line - description HERE
-*@n variable description HERE
+*@n: variable description HERE
 *Return: 0
 */
 
 #include "main.h"
 
-void print_line(void)
+void print_line(int n)
 {
 
-	int n;
+int i = 0;
+char line '_';
 
-	if (n == 0)
+if (n > 0)
+{
+	while (i < n)
 	{
-		_putchar('\n');
+		_putchar(line);
+		i++;
 	}
-	else
-	{
-	for (; n > 0; n++)
-		{
-		_putchar('_');
-		}
-	_putchar('\n');
-	}
+
+}
+_putchar('\n');
 }
