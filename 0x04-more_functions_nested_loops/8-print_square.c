c32ea3308471d08f47356f197f2da1338afa9f60
gumquat <gumquat@gmail.com>
2023-02-03 10:00:23 -0800

    :P

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+/**
+*print_square - description HERE
+*@i: variable description HERE
+*Return: 0
+*/
+
+#include "main.h"
+
+void print_square(int size)
+{
+int i = 0;
+int a = 0;
+
+if (size > 0)
+{
+	for ( i = 0; i < size; i++)
+	{
+		for (a = 0; a < size; a++)
+		{
+		_putchar('#');
+		}
+	_putchar('\n');
+	}
+}
+else
+_putchar('\n');
+}
