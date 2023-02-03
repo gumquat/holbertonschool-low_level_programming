c80182191f5842be60f8741bdc40e509f9f379a2
gumquat <gumquat@gmail.com>
2023-02-03 09:29:38 -0800

    :P

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+/**
+*print_diagonal - description HERE
+*@n: variable description HERE
+*Return: 0
+*/
+
+#include "main.h"
+
+void print_diagonal(int n)
+{
+int i = 0;
+int a = 0;
+
+if (n > 0)
+{
+	while (i < n)
+	{
+	_putchar('\');
+	_putchar('\n');
+	i++;
+	for (a = 0; a <= i; c++)
+	{
+	_putchar(' ');
+	}
+	}
+}
+else
+{
+_putchar ('\n');
+}
+}
