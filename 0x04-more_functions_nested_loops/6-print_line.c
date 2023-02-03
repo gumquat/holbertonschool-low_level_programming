0a95b40b42df3ce5584fcef1abf785c0f361040f
gumquat <gumquat@gmail.com>
2023-02-03 09:09:46 -0800

    :P

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+/**
+*print_line - description HERE
+*@n variable description HERE
+*Return: 0
+*/
+
+#include "main.h"
+
+void prnt_line(void)
+{
+
+	int n;
+
+	if (n == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (; n > 0; n++)
+		{
+		_putchar('_');
+		}
+	_putchar('\n');
+	}
+}
