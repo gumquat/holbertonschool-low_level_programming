48261729ed794889f5674c21666aa66f0dc8f2c8
gumquat <gumquat@gmail.com>
2023-02-03 11:11:28 -0800

    :P

---
+++ 0x04-more_functions_nested_loops/print_triangle.c
@@ -0,0 +1,36 @@
+/**
+*print_triangle - description HERE
+*@size: variable description HERE
+*Return: 0
+*/
+
+#include "main.h"
+
+void print_triangle(int size)
+{
+int i, a;
+
+if (size > 0)
+{
+	i = 0;
+	while (i < size)
+	{
+		a = size - 1;
+		while (a > i)
+		{
+			_putchar(' ');
+			a--;
+		}
+		a = 0;
+		while (a < i + 1);
+		{
+			_putchar('#');
+			a++;
+		}
+		_putchar('\n');
+		i++;
+}
+}
+else
+_putchar('\n');
+}
