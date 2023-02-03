61f3738dbaffabda431b56eb95ce79b1aac58e1d
gumquat <gumquat@gmail.com>
2023-02-03 10:19:15 -0800

    :P

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+/**
+*fizz_buzz - description HERE
+*Return: 0
+*/
+
+#include "main.h"
+
+void fizz_buzz(void)
+{
+
+int i = 1;
+
+for (i = 1; i <= 100; i++)
+{
+if(((i%3)||(i%5))== 0)
+printf("number= %d FizzBuzz\n", i);
+else if((i%3)==0)
+printf("number= %d Fizz\n", i);
+else if((i%5)==0)
+printf("number= %d Buzz\n", i);
+else
+printf("number= %d\n",i);
+}
+}
