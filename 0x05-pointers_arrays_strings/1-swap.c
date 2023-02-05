070016398fa924f5eabcc14a53302827a2f4ffc4
gumquat <gumquat@gmail.com>
2023-02-05 10:27:51 -0800

    :P

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 /**
-*swap - swaps the values of two integers using pointers
+*swap_int - swaps the values of two integers using pointers
 *@a; integer
 *@b: integer
 *return: void
 */
 
-void swap(int *a, int *b)
+void swap_int(int *a, int *b)
 {
 int temp = *a;
 *a = *b;
