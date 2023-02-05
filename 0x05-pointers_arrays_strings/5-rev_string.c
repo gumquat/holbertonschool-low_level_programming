a516d2bffe50e501193d424548f1f1f9875a4837
gumquat <gumquat@gmail.com>
2023-02-05 14:24:52 -0800

    :P

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,12 +15,11 @@ char temp;
 while (s[index++])
 {
 	length++;
-
+}
 	for (index = length - 1; index >= length / 2; index--)
 	{
 	temp = s[index];
 	s[index] = s[length - index - 1];
 	s[length - index - 1] = temp;
 	}
-}
 }
