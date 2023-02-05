9bc362936fff20a742e268d0f0d0fd6843e2ebe3
gumquat <gumquat@gmail.com>
2023-02-05 13:17:57 -0800

    :P

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+/**
+*_puts - prints strong from pointed location
+*@str: itsa striiing
+*Return: nothing
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != ('\0')
+	{
+	_putchar(str[i]);
+	i++;
+	}
+}
