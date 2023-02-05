d3a3f74009e74bf04b2dfa45eb95a1e96a551f8a
gumquat <gumquat@gmail.com>
2023-02-05 09:54:52 -0800

    :P

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/**
+*reset_to_98 - description HERE
+*@i: variable description HERE
+*Return: 0 or 1 or other data HERE
+*/
+
+#include "main.h"
+
+void reset_to_98(int *n)
+{
+// the value i am going to point to and set to 98	
+int i;
+//the value that is in the arguement, 98 will get passed thru it to i
+n = &i;
+*n = 98;
+}
