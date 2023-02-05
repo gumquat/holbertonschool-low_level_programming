9bac38c23cbb894289066a50d7f00f8e4f195fa7
gumquat <gumquat@gmail.com>
2023-02-05 11:20:58 -0800

    :P

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,12 @@
 /**
- * *functionnameHERE - description HERE
- * *@variableNAME: variable description HERE
- * *Return: 0 or 1 or other data HERE
- * */
+*_strlen - returns length of a string pointed to
+*@i: variable description HERE
+*Return: something
+*/
 
 #include "main.h"
 
-void/int functionNAME(void or argument HERE)
+int _strlen (char *s)
 {
 
 	return (0 or something else HERE);
