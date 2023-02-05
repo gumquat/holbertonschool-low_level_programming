744b0be77d6bf37914b26d01d2b353dbb3923fa4
gumquat <gumquat@gmail.com>
2023-02-05 10:30:13 -0800

    :P

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+/**
+ * *functionnameHERE - description HERE
+ * *@variableNAME: variable description HERE
+ * *Return: 0 or 1 or other data HERE
+ * */
+
+#include "main.h"
+
+void/int functionNAME(void or argument HERE)
+{
+
+	return (0 or something else HERE);
+}
