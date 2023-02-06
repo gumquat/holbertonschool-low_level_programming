d7de741af6be32784c049a7fe57ae749be49ceab
gumquat <gumquat@gmail.com>
2023-02-06 12:29:15 -0800

    :P

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,13 +12,13 @@ void print_array(int *a, int *n)
 int x = 0;
 
 for (x = 0; x < n; x++)
-{
+	{
 	printf("%d", a[x]);
 
-	if (x < n - 1)
-	{
+		if (x < n - 1)
+		{
 		printf(", ");
+		}
+	printf("\n");
 	}
-printf("\n");
-}
 }
