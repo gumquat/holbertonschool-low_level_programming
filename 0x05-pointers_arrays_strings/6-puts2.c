9898500a76bdd48a7c05b59e3b60cf91a5694bed
gumquat <gumquat@gmail.com>
2023-02-06 09:19:15 -0800

    :P

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+/**
+*puts2 - description
+*@str: description here
+*Return: Nothing
+*/
+
+void puts(char *str)
+{
+int i;
+int a = 0;
+
+while(str[a] != '\0')
+{
+a++
+}
+
+for (i = 0; i <j; i += 2)
+{
+_putchar(Str[i]);
+}
+_putchar('\n');
+}
