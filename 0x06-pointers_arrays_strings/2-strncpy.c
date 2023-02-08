2725727bcf1820d7021b2bef2be510038ed9b8b0
gumquat <gumquat@gmail.com>
2023-02-08 15:00:55 -0800

    :P

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,14 +3,14 @@
 *@dest: buffer zone for putting a string copy
 *@src: source of the string needed to be copied
 *@n: maximum bytes allowed to be copied
-*Return: a pointer to the new string 
+*Return: a pointer to the new string
 */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 
 int index = 0;
-int src_len;
+int src_len = 0;
 
 while (src[src_len])
 {
@@ -22,7 +22,7 @@ for (index = 0; scr[index] && index < n; index++)
 dest[index] = src[index];
 }
 
-for(index = src_len; index < n; index++)
+for (index = src_len; index < n; index++)
 {
 dest[index] = '\0';
 }
