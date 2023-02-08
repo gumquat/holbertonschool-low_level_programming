b59e0954e40c9f5628ea658f7787e41ecc3510d5
gumquat <gumquat@gmail.com>
2023-02-08 11:03:41 -0800

    :P

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,24 +2,23 @@
 *_strcat - adds a string to the end of another string
 *@dest: destination for the final string
 *@src: the source string
+*Return: compounded string inside Dest
 */
 
 char *_strcat(char *dest, char * src)
 {
 
-int butt = 0;
-int pp;
+int index = 0;
+int dest_len = 0;
 
-while (dest[butt])
-{
-butt++;
+	while (dest[index])
+	{
+		index++;
+		dest_len++;
+	}
+		for (index = 0; src[index] && index < dest_len; index++)
+		{
+			dest[dest_len++] = src[index];
+		}
+		return (dest);
 }
-
-while (src[pp] != 0)
-{
-dest[butt] = src[pp];
-butt++;
-pp++;
-}
-dest[butt] = '\0';
-return (dest);
