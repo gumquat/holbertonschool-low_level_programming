f82512ac628934cc3ceb87e45067f2168cfcab1f
gumquat <gumquat@gmail.com>
2023-02-08 15:35:13 -0800

    :P

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,27 +6,29 @@
 *Return: a pointer to the new string
 */
 
+#include "holberton.h"
+
 char *_strncpy(char *dest, char *src, int n)
 {
 
-int index = 0;
-int src_len = 0;
+	int index = 0;
+	int src_len = 0;
 
-while (src[index])
-{
-index++
-src_len++;
-}
+	while (src[index])
+	{
+	index++;
+	src_len++;
+	}
 
-for (index = 0; scr[index] && index < n; index++)
-{
-dest[index] = src[index];
-}
+	for (index = 0; src[index] && index < n; index++)
+	{
+	dest[index] = src[index];
+	}
 
-for (index = src_len; index < n; index++)
-{
-dest[index] = '\0';
-}
+	for (index = src_len; index < n; index++)
+	{
+	dest[index] = '\0';
+	}
 
 return (dest);
 }
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,3 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
 int _putchar(int c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -7,3 +12,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+
+#endif
