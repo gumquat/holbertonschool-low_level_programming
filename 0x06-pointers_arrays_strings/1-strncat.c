e1c43efc1a233acb99555299c16bae36d6cedfef
gumquat <gumquat@gmail.com>
2023-02-08 11:12:14 -0800

    :P

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+/**
+*_strcat - adds a string to the end of another string
+*@dest: destination for the final string
+*@src: the source string
+*Return: compounded string inside Dest
+*/
+
+char *_strcat(char *dest, char *src, int n)
+{
+
+int index = 0;
+int dest_len = 0;
+
+	while (dest[index])
+	{
+		index++;
+		dest_len++;
+	}
+		for (index = 0; src[index] && index < n; index++)
+		{
+			dest[dest_len++] = src[index];
+		}
+		return (dest);
+}
