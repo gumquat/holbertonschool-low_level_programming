b52eb9cec4529d4a88bf2718497bbe3980992eaf
gumquat <gumquat@gmail.com>
2023-02-08 10:36:54 -0800

    :P

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+/**
+*_strcat - adds a string to the end of another string
+*@dest: destination for the final string
+*@src: the source string
+*/
+
+char *_strcat(char *dest, char * src)
+{
+
+int butt = 0;
+int pp;
+
+while (dest[butt])
+{
+butt++;
+}
+
+while (src[pp] != 0)
+{
+dest[butt] = src[pp];
+butt++;
+}
+dest[butt] = '\0';
+return (dest);
