3678efd45b830d79867f2c6aeddc752e495d059c
gumquat <gumquat@gmail.com>
2023-02-08 12:15:24 -0800

    :P

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,14 +9,14 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int index = 0;
-int dest_len= 0;
+int dest_len = 0;
 
 while (dest[index++])
 {
 dest_len++;
 }
 
-for(index = 0; src[index] && index < n; index++)
+	for(index = 0; src[index] && index < n; index++)
 		{
 		dest[dest_len++] = src[index];
 		}
