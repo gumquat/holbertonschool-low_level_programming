add5cfd00d7fd9ab6321e2003a7be9f4f60e380a
gumquat <gumquat@gmail.com>
2023-02-09 11:22:38 -0800

    :P

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,11 +1,11 @@
 /**
 *rev_array - reverses a array
-*@a:
-*@n:
+*@a: points to an array
+*@n: integer representing number of things in an array 
 *Return: reversed array
 */
 
-void reverse_array(int *a, int n)
+void rev_array(int *a, int n)
 {
 	int temp;
 	int index;
