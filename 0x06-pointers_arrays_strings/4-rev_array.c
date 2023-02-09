ab1a736bd6c8b1dc57b6e2d6d3e378d86bb70cec
gumquat <gumquat@gmail.com>
2023-02-09 11:36:00 -0800

    :P

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -15,8 +15,8 @@ void reverse_array(int *a, int n)
 	while (index >= n / 2)
 	{
 		index--;
-		temp = a[n - 1];
-		a[n - 1] = a[index];
+		temp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
 		a[index] = temp;
 		n++;
 	}
