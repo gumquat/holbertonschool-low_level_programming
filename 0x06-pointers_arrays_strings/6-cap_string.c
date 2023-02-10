06d1ef0b50467b71228720f2b0c8f741fb5fc254
gumquat <gumquat@gmail.com>
2023-02-10 08:05:08 -0800

    :P

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,13 +11,26 @@ int index = 0;
 
 while (str[index])
 {
-	while(!(str[index] >= 'a' && str[index] <= 'z' ))
+	while (!(str[index] >= 'a' && str[index] <= 'z'))
 	{
 
-	if (str[index - 1] == ' ', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}' || index == 0)
+	if (str[index - 1] == ' ' ||
+	str[index - 1] == '\t' ||
+	str[index - 1] == '\n' ||
+	str[index - 1] == ',' ||
+	str[index - 1] == ';' ||
+	str[index - 1] == '.' ||
+	str[index - 1] == '!' ||
+	str[index - 1] == '?' ||
+	str[index - 1] == '"' ||
+	str[index - 1] == '(' ||
+	str[index - 1] == ')' ||
+	str[index - 1] == '{' ||
+	str[index - 1] == '}' ||
+	index == 0
 	str[index] -= 32;
 	index++;
 	}
 }
-	return (str);
+return (str);
 }
