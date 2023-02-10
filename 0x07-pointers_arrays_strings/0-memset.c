f6c6cdcec47dfe0b20d6782ffad633c75c606ad8
gumquat <gumquat@gmail.com>
2023-02-10 13:41:35 -0800

    :P

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+/**
+*memset - fills memory with a constant byte
+*@s: string thats pointed to  
+*@b: string
+*@n: variable thats unassaigned
+*Returns: a pointer to the memory asrea s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	unsigned int i;
+
+	 for (i = 0; i < n i++)
+ 	{
+	s[i] = b;
+	}
+	 return [s];
+}
