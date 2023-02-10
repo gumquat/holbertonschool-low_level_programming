e961b28e9ef75f7d7f7bf91b2f2c497ece49b0e5
gumquat <gumquat@gmail.com>
2023-02-10 10:41:38 -0800

    :P

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,7 +8,7 @@ char *leet(char *s)
 {
 	char *sclone = s;
 	char letters[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	char numbers[5] = {'4', '3', '0', '7', '1'};
+	char numbers[5] = {4, 3, 0, 7, 1};
 	int index = 0;
 
 	while (*s)
