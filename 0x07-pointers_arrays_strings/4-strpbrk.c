c38f2df7811e982dda82c0fbd9e3cf654dc75a5d
gumquat <gumquat@gmail.com>
2023-02-11 10:34:17 -0800

    :P

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+/**
+*_strpbrk - searches a string for any of a set of bytes
+*@s: input
+*@accept: input
+*Return: pointer to s taht matches bytes in accept
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int n;
+
+	for (i = 0; s[i] != '\0'; n++)
+	{
+		if (s[i] == accept[n])
+		{
+			return (s + i);
+		}
+	}
+	return (NULL);
+}
