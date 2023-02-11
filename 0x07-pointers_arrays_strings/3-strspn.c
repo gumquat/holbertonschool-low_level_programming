e9fff8ba7bc3b96d70c293ae545243bd55c2ddce
gumquat <gumquat@gmail.com>
2023-02-11 09:46:45 -0800

    :P

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+/**
+ *_strpn - gets the length of a prefix substring
+ *@s: input
+ *@accept: input
+ *Return: 0 is success
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, n, check;
+	unsigned int value = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		check = 0;
+
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (accept[n] == s[i])
+			{
+				value++;
+				check = 1;
+			}
+		}
+	}
+}
