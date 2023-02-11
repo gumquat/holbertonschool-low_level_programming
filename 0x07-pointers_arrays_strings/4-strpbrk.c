8ef15fd10ecfe98415f2ad788c214353d4af5daa
gumquat <gumquat@gmail.com>
2023-02-11 10:37:47 -0800

    :P

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,12 +10,15 @@ char *_strpbrk(char *s, char *accept)
 	int i;
 	int n;
 
-	for (i = 0; s[i] != '\0'; n++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == accept[n])
+		for (n = 0; accept[n] != '\0'; n++)
 		{
+			if (s[i] == accept[n])
+			{
 			return (s + i);
+			}
 		}
 	}
-	return (0);
+return(0);
 }
