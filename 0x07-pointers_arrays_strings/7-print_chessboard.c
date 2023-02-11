a7b8178d90ec11a2a57606404deefa3df26fdfac
gumquat <gumquat@gmail.com>
2023-02-11 11:12:40 -0800

    :P

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+/**
+*print_chessboard - Print a whole as mf chessboard i guess
+*@a: rows and columns in an array of pieces
+*Return: cool ah chessboard
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int i, c;
+	
+	for (i = 0; i < 8; i++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[i][c]);
+		}
+		_putchar('\n');
+	}
+}
