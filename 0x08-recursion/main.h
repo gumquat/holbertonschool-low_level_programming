73687b271b32b46f29129bd87a90e577ece31717
gumquat <gumquat@gmail.com>
2023-02-15 11:52:26 -0800

    :P

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,9 @@
-#ifdef HOLBERTON_H
-#define HOLBERTON_H
+#ifdef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 int _putchar(char c);
-on(char s);
-void _print_rev_recursion(chars);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
