7861ceef0d3558cf291bd221acb08c543d3e1760
gumquat <gumquat@gmail.com>
2023-02-15 13:41:15 -0800

    :P

---
+++ 0x08-recursion/5-sqrt-recursion.c
@@ -0,0 +1,36 @@
+/**
+ *_sqrt_recursion - returns the natural square root of a number
+ *@n: integer
+ *Return: sum
+ */
+
+/*prototypes on line below*/
+int _sqrt(int n, int i);
+
+
+
+int _sqrt_recursion(int n)
+{
+
+	return (_sqrt(n, 1));
+
+}
+
+int _sqrt(int n, int i)
+{
+
+	int sqrt = i * i;
+
+	if (sqrt > n)
+	{
+		return (-1);
+	}
+
+	if (sqrt == n)
+	{
+		return (i);
+	}
+
+	return (_sqrt(n, i + 1));
+
+}
