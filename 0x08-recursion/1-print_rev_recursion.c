023dfcfa073148e6b3f5b923c09cc517b8d86e92
gumquat <gumquat@gmail.com>
2023-02-15 12:17:38 -0800

    :P

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+/**
+ *print_rev_recursion - prints string in reverse
+ *@s: string getting passed thru
+ *Return: Silly little string haha reverse reverse!
+ */
+
+#include "main.h"
+
+void _print_rev_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return;	
+	}
+	else
+	{
+		s++;
+		_prints_rev_recursion(s);
+		s--;
+		_putchar(*s);
+	}
+
+}
