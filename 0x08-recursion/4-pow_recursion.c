b4edc18c22a05182d2d69f707950cbd1f73a3a58
gumquat <gumquat@gmail.com>
2023-02-15 13:06:29 -0800

    :P

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+/**
+ *_pow_recursion - returns the value x raised to the power y
+ *@x: integer
+ *@y: integer
+ *Returns: sum of x to power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+
+}
