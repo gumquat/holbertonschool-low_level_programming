f84f9093ccff66e3daf2d172abf2e8c61fdf6045
gumquat <gumquat@gmail.com>
2023-02-17 12:50:46 -0800

    :P

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+/**
+ *main - prints number of arguments pass thru it
+ *@argc: arguement counter
+ *@argv: arguement vector
+ *Return 0 as always
+ */
+ 
+#include "main.h"
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+
+int index;
+
+for (index = 0; index < argc; index++;)
+{
+	printf("%s\n", argv[index]);
+}
+return (0);
+}
