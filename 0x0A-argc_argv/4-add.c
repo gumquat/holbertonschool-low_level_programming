2041a93785c10a10fa34685eea8e8f403945dea3
gumquat <gumquat@gmail.com>
2023-02-17 13:26:24 -0800

    :P

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+/**
+ *main - adds two numbers 
+ *@argc: arguement counter
+ *@argv: arguement vector
+ *Return: 0 as usual
+ */
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+
+int index;
+int var1 = 0;
+int var2 = 0;
+int sum = 0;
+
+	if (argc < 1)
+	{
+	printf("0\n");
+	}
+	else
+	{
+		for (index = 0; index < argc; index++;)
+		{
+			if (argv[index] < 48 || argc[index] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}			
+		}
+		sum += atoi(argv[index]);
+	}
+printf("%d\n", sum);
+return (0);
+
+}
