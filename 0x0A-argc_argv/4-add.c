f3e92f80d96072a9516ae3433b411e59db1e6463
gumquat <gumquat@gmail.com>
2023-02-18 12:13:50 -0800

    :P

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,5 @@
 /**
- *main - adds two numbers 
+ *main - adds two numbers
  *@argc: arguement counter
  *@argv: arguement vector
  *Return: 0 as usual
@@ -9,24 +9,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 
 int index;
 int var = 0;
 int sum = 0;
 
-	for (index = 1; i < argc; index++)
+	for (index = 1; index < argc; index++)
 	{
-		for (var = 0; argv[index][var]; d++)
+		for (var = 0; argv[index][var]; var++)
 		{
-			if (argv[index][variable] < '0' || argv[index][var] > '9')
+			if (argv[index][var] < '0' || argv[index][var] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		sum += atoi(argv[i]);
+		sum += atoi(argv[index]);
 	}
 	printf("%d\n", sum);
 	return (0);
