c09d0da2e9f0df23e0997d294b8196a3c9f7b440
gumquat <gumquat@gmail.com>
2023-02-20 11:24:14 -0800

    :P

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,17 +15,13 @@ char *create_array(unsigned int size, char c)
 unsigned int index;
 char *pointer;
 
-/*sets pointer equal to memory value allocated to the string*/
 pointer = malloc(size * sizeof(char));
 
-/*if size is 0 return NULL*/
 if (size == 0)
 	{
 	return (NULL);
 	}
 
-/*this creates an array of characters by passing the c from the parameter
- * into an array called index*/
 for (index = 0; index < size; index++)
 	{
 	pointer[index] = c;
