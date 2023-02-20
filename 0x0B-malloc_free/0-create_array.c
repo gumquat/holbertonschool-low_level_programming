00750b2c34daed3504448745871f6082b61488e0
gumquat <gumquat@gmail.com>
2023-02-20 11:23:06 -0800

    :P

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,30 +1,35 @@
 /**
  *create_array - creates an array of char and initializes with a specific char
  *@size: size of array
- *@c: string
+ *@c: char to fill in the array
  *Return: null if size = 0
  */
 
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 char *create_array(unsigned int size, char c)
 {
 
-unsigned int butt;
+unsigned int index;
 char *pointer;
 
+/*sets pointer equal to memory value allocated to the string*/
+pointer = malloc(size * sizeof(char));
+
+/*if size is 0 return NULL*/
 if (size == 0)
 	{
 	return (NULL);
 	}
-/*this line sets pointer equal to the memory allocated to the string*/
-pointer = malloc(size * sizeof(char));
 
+/*this creates an array of characters by passing the c from the parameter
+ * into an array called index*/
 for (index = 0; index < size; index++)
-{
-s[i] = c;
-}
+	{
+	pointer[index] = c;
+	}
 
 return (pointer);
 
