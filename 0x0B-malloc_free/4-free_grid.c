675d1c2e9ce33f45aa6c2bd403b4e69c39b30e59
gumquat <gumquat@gmail.com>
2023-02-20 15:52:17 -0800

    :P

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,12 +1,14 @@
-#include "holberton.h"
-#include <stdlib.h>
-
 /**
 *free_grid - frees a 2 dimensional grid 
 *@grid: the grid space
 *@height: height of the grid
 *Return: nothin
 */
+
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 void free_grid(int **grid, int height)
 {
 	int i;
