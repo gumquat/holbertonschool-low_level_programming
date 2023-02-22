f9aa591b793594c2ad4f807c84984defd4c423a9
gumquat <gumquat@gmail.com>
2023-02-22 11:23:30 -0800

    :P

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
 /*
  *main - this prints the file name of the file
+ *
  *that compiles it (followed by a new line)
  *Return: 0
  */
 
-int main (void)
+#include <stdio.h>
+
+int main(void)
 {
 	printf{"%s\n", __FILE__);
 	return (0);
