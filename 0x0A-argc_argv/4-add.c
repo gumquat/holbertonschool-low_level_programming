d4e891453ec5262b2dde24aa1cec135a5d3b8cf6
gumquat <gumquat@gmail.com>
2023-02-21 16:54:34 -0800

    :P

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,17 +9,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*this function passes two items in its parameter
+ *the first is an integer that will act as the 
+ *argument counter, and the second is a pointer to the
+ *argument vector*/
 int main(int argc, char *argv[])
 {
 
-int index;
-int var = 0;
-int sum = 0;
+	/*the following lines initialize three integers, the first
+	 * will be used to count thru our loops, the second will
+	 * hold the value of a variable, and the third will hold 
+	 * the final result of our math*/
+	int index;
+	int var = 0;
+	int sum = 0;
 
+	/*the first for loop will iterate thru the amount of arguments
+	 * that are inside argc, making sure we go thru all of them*/
 	for (index = 1; index < argc; index++)
-	{
+	{	
+		/*the second for loop will iterate through the 2 dimensional 
+		 * array hel by argv
+		 * index will pass thru rows, and var will pass
+		 * thru columns*/
 		for (var = 0; argv[index][var]; var++)
 		{
+			/*this if statement will check if any value passing
+			 * thru the above loop contains a symbol that is not
+			 * a digit*/
 			if (argv[index][var] < '0' || argv[index][var] > '9')
 			{
 				printf("Error\n");
