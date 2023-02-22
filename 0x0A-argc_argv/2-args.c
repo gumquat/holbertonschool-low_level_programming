c4f8cf6e032975558137bb4d207ce42b3d2f9495
gumquat <gumquat@gmail.com>
2023-02-21 16:40:58 -0800

    :P

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,11 +8,23 @@
 #include "main.h"
 #include <stdio.h>
 
+/*this function passes two items in its parameter
+ *the first is an integer taht will act as the 
+ *argument counter, and the second is a pointer to the
+ *argument vector*/
 int main(int argc, char *argv[])
 {
-
+	/*this line initializes an integer to be used
+	 * a counter that will increment durng our loops*/
 	int index;
 
+	/*this loop will iterate and print through every 
+	 * value stored in addresses inside of argv, because you
+	 * can access specific values in the memory of an array
+	 * with the following syntax: array[location_number]
+	 * in this case we are using index to specify the location
+	 * in the array we want printed, and then increment it
+	 * and loop over again to print the following value*/
 	for (index = 0; index < argc; index++)
 	{
 		printf("%s\n", argv[index]);
