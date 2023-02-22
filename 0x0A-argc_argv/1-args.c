5709c976f9a85aaeb9c7cbe01435f4c4af618fad
gumquat <gumquat@gmail.com>
2023-02-21 16:34:36 -0800

    :P

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -16,9 +16,12 @@ int main(int argc, char *argv[])
 {
 	/*this first line makes the function ignore an error*/
 	(void) argv;
-	/*this line will print 
-	 *
-	 */
+	/*this line will print the number of arguments that 
+	 *exist inside of argc, you'll notice that it says -1
+	 *that accounts for all arguments, as the number of 
+	 *arguments are counted starting at 0, and the number value of
+	 *argc needs to be reduced by 1 when called to accurately
+	 *represent the number of arguments it actually holds*/
 	printf("%d\n", argc - 1);
 	return (0);
 }
