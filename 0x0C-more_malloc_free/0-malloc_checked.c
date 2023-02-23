187e2fbe7123e0d0f80e5d203c97429184619579
gumquat <gumquat@gmail.com>
2023-02-23 12:53:26 -0800

    :P

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+/*malloc_checked - allocated memory so long as there is enough
+ *@b: the size of memory to allocate
+ *Return: zilch
+ */
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int b)
+{
+
+
+
+
+
+}
