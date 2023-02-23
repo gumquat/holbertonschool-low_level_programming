fd08d91c97fa9d10e01fd7e2b91ee15defc3ab84
gumquat <gumquat@gmail.com>
2023-02-23 13:04:09 -0800

    :P

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,8 +10,19 @@
 void *malloc_checked(unsigned int b)
 {
 
+/*this line makes a pointer with no data type*/ 
+void *pointer;
 
+/*this line sets the value of the pointer to a 
+ * newly allocated (malloc'd) spot in memory that is 
+ * the size of the integer b*/
+pointer = malloc(b);
 
+/*this checks if the pointer is NULL
+ * and if it is exit's with a status value of 98*/
+if (p == NULL)
+	exit(98);
 
+return (pointer);
 
 }
