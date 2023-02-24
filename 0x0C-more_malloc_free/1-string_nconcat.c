70000a5b6821c4160b442527d86a2bbdc9e062e6
gumquat <gumquat@gmail.com>
2023-02-23 16:54:10 -0800

    :P

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,42 +13,32 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 
-	int s1Len = 0;
-        int s2Len, bufferLen, index, index2 = 0;
+	int s1Len, s2Len, bufferLen, index, index2 = 0;
 	char *pointer;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
+
 	while (s1[s1Len])
-	{
 		s1Len++;
-	}
+
 	while (s2[s2Len])
-	{
 		s2Len++
-	}
 
 	bufferLen = s1Len + s2Len;
 
 	pointer = malloc((sizeof(char) * bufferLen) + 1);
 
 	if (pointer == NULL)
-	{
-	return (NULL);
-	}
+		return (NULL);
 
 	while (index < bufferLen)
 	{
 		if (index <= s1Len)
-		{
 			pointer[index] = s1[index];
-		}
 		if (index >= s1Len)
 		{
 			pointer[index] = s2[index2];
