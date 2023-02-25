f1f73bdd5368b23be81e5421dc70f5530fd87038
gumquat <gumquat@gmail.com>
2023-02-25 10:01:46 -0800

    :P

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 
-	int s1Len, s2Len, bufferLen, index, index2 = 0;
+	int s1Len, s2Len, bufferLen, index = 0;
 	char *pointer;
 
 	if (s1 == NULL)
@@ -21,32 +21,38 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s2 == NULL)
 		s2 = "";
-
+	/*this line gets the length of the first string
+	*and stores that value in s1Len*/
 	while (s1[s1Len])
 		s1Len++;
-
+	/*this line gets the length of the second string
+	* and stores that value in s2Len*/
 	while (s2[s2Len])
 		s2Len++;
-
+	/*this value checks if the second string is larger than n
+	*if it is, we set the value of s2Len to the value of n to shorten it
+	*because only n bytes of s2 are allowed to be put in memory*/
+	if (s2Len > n)
+		s2Len = n;
+	/*this sets bufferLen to the size of s1 and s2 together*/
 	bufferLen = s1Len + s2Len;
-
-	pointer = malloc((sizeof(char) * bufferLen) + 1);
-
+	/*this line allocates an area of memory with the exact amount we 
+	 *need to store string 1 and n amount of bytes of string 2
+	 *as well as an extra space to put a null character at the end*/
+	pointer = malloc(sizeof(char) * (bufferLen + 1));
+	/*if our pointer shows that there is no memory there, return NULL*/
 	if (pointer == NULL)
 		return (NULL);
+	/*this line puts the string from s1 into newly allocated memory*/
+	for (index = 0; s1[index]; index++)
+		pointer[s1Len++] = s1[index];
+	/*this string fills the rest of the allocated memory with 
+	 * the string from s2, and makes sure we don't write over the final spot
+	 * ecause we want to put a null character there*/
+	for (index = 0; s2[index] && index < bufferLen; index++)
+		pointer[s1len++] = s2[index];
 
-	while (index < bufferLen)
-	{
-		if (index <= s1Len)
-			pointer[index] = s1[index];
-		if (index >= s1Len)
-		{
-			pointer[index] = s2[index2];
-			index2++;
-		}
+	pointer[s1len] = '\0';
 
-		index++;
-	}
-	pointer[index] = '\0';
-	return (pointer);
+	return (pointer);	
 }
