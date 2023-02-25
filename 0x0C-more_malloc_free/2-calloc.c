e642df9f81be6bf61bfdd94a7fd343856bbaf89c
gumquat <gumquat@gmail.com>
2023-02-25 15:51:36 -0800

    :P

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+/**_calloc - allocates memory for an array using malloc
+ * @nmemb: number of members
+ * @size: size of thing passed
+ * Return: null on failures*/
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+int index = 0;
+int length = 0;
+char *pointer;
+
+if (nmem == 0 || size == 0)
+	return (NULL);
+
+length = nmemb * size;
+pointer = malloc(length);
+
+if (pointer == NULL)
+	return (NULL);
+
+	while (index < length)
+	{
+		pointer[index] = 0;
+		index++;
+	}
+}
