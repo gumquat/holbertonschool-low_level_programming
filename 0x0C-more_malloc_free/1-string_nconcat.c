f25822c812f8b3a595ac4073615e1e61fe23d5c2
gumquat <gumquat@gmail.com>
2023-02-27 10:40:45 -0800

    :P

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,49 @@
+/**
+ *string_nconcat - concatonates two strings using pointers
+ *@s1: string 1
+ *@s2: string 2
+ *@n: max length of string 2 that can be moved
+ *Return: pointer
+ */
+
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-/**
- *  * string_nconcat - Concatenates two strings
- *   * @s1: The first string
- *    * @s2: The second string
- *     * @n: The maximum number of bytes of s2 to concatenate to s1
- *      *
- *       * Return: a pointer to the concatenated string in memory
- *        */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int len = n, index;
+
+	unsigned int s1Len = 0, s2Len = 0, bufferLen, index = 0;
+	char *pointer;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
+	while (s1[s1Len])
+		s1Len++;
+	while (s2[s2Len])
+		s2Len++;
+	if (s2Len > n)
+		s2Len = n;
 
-	for (index = 0; s1[index]; index++)
-		len++;
+	bufferLen = s1Len + s2Len;
 
-	concat = malloc(sizeof(char) * (len + 1));
+	pointer = malloc(sizeof(char) * (bufferLen + 1));
+	if (pointer == NULL)
 
-	if (concat == NULL)
 		return (NULL);
-
-	len = 0;
-
+	s1Len = 0;
 	for (index = 0; s1[index]; index++)
-		concat[len++] = s1[index];
-
-	for (index = 0; s2[index] && index < n; index++)
-		concat[len++] = s2[index];
-
-	concat[len] = '\0';
-	return (concat);
+	{
+		pointer[s1Len] = s1[index];
+		s1Len++;
+	}
+	for (index = 0; s2[index] && index <= bufferLen; index++)
+	{
+		pointer[s1Len++] = s2[index];
+		s2Len++;
+	}
+	pointer[s1Len] = '\0';
+	return (pointer);
 }
-
