46289cf7b2ceacd7b98e599266464e7aa6ab4320
gumquat <gumquat@gmail.com>
2023-02-27 12:01:53 -0800

    :P

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,15 +14,15 @@ int *array_range(int min, int max)
 	int index = 0;
 	int *pointer = 0;
 
-	if (min > max) 
+	if (min > max)
 		return (NULL);
-	
+
 	pointer = malloc((sizeof(int) * (max - min)) + sizeof(int));
 
 	if (pointer == NULL)
 		return (NULL);
 
-	while(min <= max)
+	while (min <= max)
 	{
 	pointer[index] = min;
 	index++;
