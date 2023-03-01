cc64f41e0fd7ed64b75fd2352b271734e7200cc2
gumquat <gumquat@gmail.com>
2023-03-01 11:28:05 -0800

    :P

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,13 @@
 /**
- *
- *
- *
+ *init_dog - initializes a variable of type struct dog
+ *@d: the dog to be initialized
+ *@name: the name of the dog getting passed in
+ *@age: the age of the dog getting passed in
+ *@owner: the name of the owner of the dog getting passed in
  */
+
+#include "dog.h"
+#include <stdlib.h>
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
