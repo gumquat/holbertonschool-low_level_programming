8a9d86d89526816705185a50dd5a2427ec465476
gumquat <gumquat@gmail.com>
2023-03-03 09:18:47 -0800

    :P

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "function_pointers.h"
 /**
  *print_name - function that prints a name
  *@name: name to be printed
@@ -7,6 +7,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (f && name)
-		f(name);
+	if (f == NULL || name == NULL)
+		return;
+
+	f(name);
 }
