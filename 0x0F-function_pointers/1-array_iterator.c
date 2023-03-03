6854e4de102158f793da49fd7b7cc37ce5a81aab
gumquat <gumquat@gmail.com>
2023-03-03 09:30:08 -0800

    :P

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ *@array_iterator - ...
+ *@array: array passed in
+ *@size: size of array
+ *@action: pointer to a function that returns nothing & takes an int
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+{
+	int index = 0;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	if (size == 0)
+		return;
+
+	while (index < size)
+	{
+	action(array[index]);
+	index++;
+	}
+}
