e4891d7ce8576d6610d5a9af2de92870e83719b9
gumquat <gumquat@gmail.com>
2023-03-03 11:36:05 -0800

    :P

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "calc.h"
+
+/**
+ *get_op_func - contain the function that selects the correct function 
+ *to perform the operation asked by the user
+ *@s: operator passed as an argument
+ *Return: pointer to the needed function or NULL if bad garbage doodoo
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	if (strlen(s) != 1)
+		return (NULL);
+
+	while()
+	{
+	}
+
+	return (ops[i + 1]);
+}
