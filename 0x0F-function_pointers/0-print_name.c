95d4314acf05e311989607d4e218921c11906db9
gumquat <gumquat@gmail.com>
2023-03-03 09:11:02 -0800

    :P

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,14 @@
 #include "main.c"
-#include <stdlib.h>
+/**
+ *print_name - function that prints a name
+ *@name: name to be printed
+ *@f: function pointer to a function that takes a character pointer
+ */
 
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL || name == NULL)
+		return;
+	
+	f(name);
+}
