ed6722af81a2de784431739bcadac90c79b98e1e
gumquat <gumquat@gmail.com>
2023-03-04 14:51:53 -0800

    :P

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,11 +10,18 @@ int main (int argc, int *argv[])
 {
 	int num1;
 	int num2;
-	int (*operator)(int, int);
+	int (*op)(int, int);
+	operator = argv[2];
 
 	if(argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+
+	op = get_op_function(operator);
 }
