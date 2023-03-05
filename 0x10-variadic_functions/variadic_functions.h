c9910b52ae4ad87a1e2c45280a2d0fa19f10805a
gumquat <gumquat@gmail.com>
2023-03-05 11:21:06 -0800

    :P

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+int _putchar(char c);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
