500119e42290eba76a6a72621e4ccc6624a42c8e
gumquat <gumquat@gmail.com>
2023-03-05 11:52:04 -0800

    :P

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,7 +8,7 @@
 #include <stdarg.h>
 
 int _putchar(char c);
-void sum_them_all(const char *seperator, const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
 void print_strings(const char *seperator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
