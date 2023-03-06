ca841d9f215f38243985218743ccc0395cc14675
gumquat <gumquat@gmail.com>
2023-03-06 08:58:52 -0800

    :P

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,15 +3,15 @@
  *main - where it all comes together to do math stuff
  *@argc: counts the arguments in command line
  *@argv: this kills the crab
- *@Return: at this point only god knows
+ *Return: at this point only god knows
  */
 
 int main(int argc, int *argv[])
 {
 	int num1;
 	int num2;
-	int (*character)(int, int);
-	char *operator;
+	int (*operator)(int, int);
+	char *poop;
 
 	if (argc != 4)
 	{
@@ -24,17 +24,17 @@ int main(int argc, int *argv[])
 	/*where the first 1 is equal to argv[1], the + is argv[2], etc*/
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	character = argv[2];
-	operator = get_op_function(operator);
+	poop = argv[2];
+	operator = get_op_function(poop);
 
-	if (operator = NULL)
+	if (operator == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 	/*operator compared with '/' and if equal to 0 (aka are the same thing)*/
 	/*also the same comparison with '%', and if num2 is zero*/
-	if ((strcmp(character, "/") == 0 || (strcmp(character, "%")) == 0) && num2 == 0)
+	if ((strcmp(poop, "/") == 0 || (strcmp(poop, "%")) == 0) && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
