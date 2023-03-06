55287e069f6aada7b066294dc1cc4e1d96ab3180
gumquat <gumquat@gmail.com>
2023-03-06 09:25:42 -0800

    :P

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -6,23 +6,22 @@
 #include <stddef.h>
 #include <string.h>
 
+/**
+ *struct op - hold operator symbols
+ *@op: the operator
+ *@f: the function
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-
-
-/**
- *struct op - holds operator symbols
- *@op: the operator
- *@f: the function
- */
-typedef struct op
-{
-char *op;
-int (*f)(int a, int b);
-} opt_t;
 
 #endif
