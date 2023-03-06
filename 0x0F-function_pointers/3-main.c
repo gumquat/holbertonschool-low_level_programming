f8f8c04b9b0ff934afb0037883dc177dc132a88e
gumquat <gumquat@gmail.com>
2023-03-06 10:27:17 -0800

    :P

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,10 +6,9 @@
  *Return: at this point only god knows
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int num1;
-	int num2;
+	int num1, num2;
 	char *poop;
 	int (*operator)(int, int);
 
@@ -19,21 +18,18 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	/*the numbers here represent what is getting typed in*/
-	/*as the argument, aka 1 + 1*/
-	/*where the first 1 is equal to argv[1], the + is argv[2], etc*/
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
+
 	poop = argv[2];
-	operator = get_op_function(poop);
+	operator = get_op_func(poop);
 
 	if (operator == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	/*operator compared with '/' and if equal to 0 (aka are the same thing)*/
-	/*also the same comparison with '%', and if num2 is zero*/
+
 	if ((strcmp(poop, "/") == 0 || (strcmp(poop, "%")) == 0) && num2 == 0)
 	{
 		printf("Error\n");
@@ -41,6 +37,5 @@ int main(int argc, char **argv)
 	}
 
 	printf("%d\n", operator(num1, num2));
-
 	return (0);
 }
