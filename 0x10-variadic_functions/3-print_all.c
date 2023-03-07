cd06c74a4f3b90e576f54cd066c0f84183cae686
gumquat <gumquat@gmail.com>
2023-03-06 17:06:03 -0800

    :P

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/**
+ *print_all - function that prints anything
+ *@format: list of the type arguments passed to the function
+ */
+
+void print_all(const char* const format, ...)
+{
+
+}
