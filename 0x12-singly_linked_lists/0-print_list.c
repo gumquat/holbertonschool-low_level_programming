4c4cc8a8c4777249778aae24c875f4ee6d7c203c
gumquat <gumquat@gmail.com>
2023-03-15 12:09:15 -0700

    :P

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,31 @@
 #include "lists.h"
 /**
- *
- *
- *
- *
- *
+ *print_list - prints elements from list
+ *@h: head of linked list that needs to be printed
+ *Return: number of nodes
  */
+
+size_t print_list(const list_t *h)
+{
+	int index = 0;
+
+	const list_t *temp = h;
+
+	while (temp)
+	{
+		if (!(temp -> str))
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", temp->len, temp->str);
+		}
+
+		index++;
+		temp = temp->next;
+	}
+
+	return (index);
+
+}
