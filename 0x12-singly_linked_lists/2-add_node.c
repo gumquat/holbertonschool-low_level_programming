be8ee2795de66dfeda0b4ef95a13061053639ed7
gumquat <gumquat@gmail.com>
2023-03-16 09:23:22 -0700

    :P

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,12 +11,13 @@ list_t *add_node(list_t **head, const char *str)
 {
 
 	list_t *newNode;
+
 	newNode = malloc(sizeof(list_t));
 
 	if (!newNode)
 	{
 		free(newNode);
-		return(NULL);
+		return (NULL);
 	}
 
 	newNode->next = *head;
