663e0eb73c6ae16cf2f04db621cdaf31013539fc
gumquat <gumquat@gmail.com>
2023-03-16 08:07:24 -0700

    :P

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *list_len - returns the number of elements in a linked list
+ *@h: pointer to header of a linked list
+ *Return: number of elements in the the linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	int index = 0;
+	const list_t *temp = h;
+
+	while (temp)
+	{
+		index++;
+		temp = temp->next;
+	}
+	
+	return (index);
+}	
