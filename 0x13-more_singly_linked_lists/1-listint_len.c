a7979d16c4329f30de26d3701cb54fcc915c8424
gumquat <gumquat@gmail.com>
2023-03-17 16:14:59 -0700

    :P

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *listint_len - returns number of elements in listint_t list
+ *@h: pointer to header of a list
+ *Return: number of elements in list_t
+ */
+
+size_t lintint_len(const listint_t *h)
+{
+	const listint_t *temp = h;
+	int nodes = 0;
+
+	while (temp)
+	{
+		nodes++
+		tempt = tempt->next;
+	}
+
+	return (nodes);
+}
