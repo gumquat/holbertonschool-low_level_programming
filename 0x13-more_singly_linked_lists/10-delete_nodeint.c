4fbf9d536b89df6b3a05b9267eb103c599b28c7b
gumquat <gumquat@gmail.com>
2023-03-18 15:18:47 -0700

    :P

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,14 +16,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 
 	if (index == 0)
-		{
-			node_to_delete = *head;
-			*head = (*head)->next;
-			free(node_to_delete);
+	{
+		node_to_delete = *head;
+		*head = (*head)->next;
+		free(node_to_delete);
 
 		return (1);
-		}
-	
+	}
+
 			while (temp && index != 0)
 			{
 
