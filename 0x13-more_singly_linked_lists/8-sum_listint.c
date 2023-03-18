cd861f65cf94a975eec71d7648949753f48ff2e7
gumquat <gumquat@gmail.com>
2023-03-18 13:55:01 -0700

    :P

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *sum_listint - returns thesum of all data (n) stored in the nodes of a list
+ *@head: pointer to the head of a list
+ *Return: sum of all data
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *temp = head;
+	int sum = 0;
+
+	whiel (temp)
+	{
+		sum = sum + temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
