6fd8f9f4f04630fb6e2baad2ca86f847557d08c3
gumquat <gumquat@gmail.com>
2023-03-19 17:34:53 -0700

    :P

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* binary_to_uint - convert a binary number to an unsigned int
+* @b: string of 0 and 1 characters
+* Return: converted number or 0 on failure
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int index, sum, count = 0;
+
+	if (!b)
+		return (0);
+
+	while (b[index])
+	{
+		if (b[index] != '0' && b[index] != '1')
+			return (0);
+
+		if (b[index] == '1')
+			sum += 1 << count;
+
+		count++;
+		index++;
+	}
+
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/_bin2int.c
@@ -0,0 +1,12 @@
+/**
+ *_bin2int - converts a binary number to an unsinged integer
+ *@num1: number
+ *@num2: power to apply to num1
+ *Return: sum
+ */
+
+unsigned int _bin2int(unsigned int num1, unsigned int num2)
+{
+	unsigned int a = num1;
+	unsigned int index = 1;
+}
