a058331b73b67f429dcb22b31b95adc69960e06f
gumquat <gumquat@gmail.com>
2023-03-20 12:21:38 -0700

    :P

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,9 +9,9 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(long) * 8)
-			return (-1);
+		return (-1);
 
 	*n = (1 << (unsigned long)index) | *n;
-			return (1);
+		return (1);
 }
 
