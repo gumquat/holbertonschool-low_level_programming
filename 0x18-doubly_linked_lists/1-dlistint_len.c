3a84511ee5a247dde431364fbd0aadd944a3ee03
gumquat <gumquat@gmail.com>
2023-03-22 09:22:36 -0700

    :P

--- 0x18-doubly_linked_lists/1-dlistint_len.c
+++ 0x18-doubly_linked_lists/1-dlistint_len.c
@@ -3,7 +3,7 @@
 /**
  *dlistint_len - returns number of elements in a linked list
  *@h: pointer to the head of a list
- *@Return: number
+ *Return: number
  */
 
 size_t dlistint_len(const dlistint_t *h)
