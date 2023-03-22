2961d0f1d03dbe04d639afe8ccbdafe5f0164eb5
gumquat <gumquat@gmail.com>
2023-03-22 09:21:00 -0700

    :P

---
+++ 0x18-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *dlistint_len - returns number of elements in a linked list
+ *@h: pointer to the head of a list
+ *@Return: number
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp = h;
+	int numNode = 0;
+
+	while (temp)
+	{
+		numNode++;
+		temp = temp->next;
+	}
+	return (numNode);
+}
