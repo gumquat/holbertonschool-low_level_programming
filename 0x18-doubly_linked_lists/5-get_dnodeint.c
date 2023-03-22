c8452b28c38b618bf7fb4aad509cb4c484351d80
gumquat <gumquat@gmail.com>
2023-03-22 11:00:05 -0700

    :P

--- 0x18-doubly_linked_lists/5-get_dnodeint.c
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -9,10 +9,10 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *temp = *head;
+	dlistint_t *temp = head;
 	unsigned int location = 0;
 
-	while (temp)
+	while (temp && location index)
 	{
 		if(location == index)
 		{
