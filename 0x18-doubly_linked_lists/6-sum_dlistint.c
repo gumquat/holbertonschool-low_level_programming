421c6561dc861fe1d74b8ceff803d0c88bed2961
gumquat <gumquat@gmail.com>
2023-03-22 11:30:23 -0700

    :P

---
+++ 0x18-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *sum_dlistint - returns the sum of data in a node 
+ *@head: pointer the head of a list
+ *Return: data
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+	int sum = 0;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+
+	return (sum);
+}
