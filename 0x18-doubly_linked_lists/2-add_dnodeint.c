293a51a9eaf896160af5336330302058e07b5b24
gumquat <gumquat@gmail.com>
2023-03-22 09:42:38 -0700

    :P

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- *add_dnodeint - adds a new node at the beggining of a dlistint_t list
- *@head: pointer to the head of a node list
- *@n: location index of an element
- *Return:
- */
+*add_dnodeint - adds a new node at the beggining of a dlistint_t list
+*@head: pointer to the head of a node list
+*@n: location index of an element
+*Return: address of new element
+*/
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
