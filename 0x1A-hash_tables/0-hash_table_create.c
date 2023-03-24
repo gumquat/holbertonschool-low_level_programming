485aa62347ef1b3f610eb456dc0e10651be14319
gumquat <gumquat@gmail.com>
2023-03-24 12:34:42 -0700

    :P

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,4 @@
-#include "has_tables.h"
+#include "hash_tables.h"
 
 /**
  *hash_table_create - create a hash table
@@ -11,9 +11,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	unsigned long int index = 0;
 
-	hash_table_t *hash_table = malloc(sizeof(has_table_t));
+	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 
-	if (!has_table || !size)
+	if (!hash_table || !size)
 		return (NULL);
 
 	has_table->array = malloc(sizeof(hash_node_t *) * size);
