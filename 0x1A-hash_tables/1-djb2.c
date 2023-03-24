e3a6c12551abfe0378af08dbd030bbb9f3acf677
gumquat <gumquat@gmail.com>
2023-03-24 13:03:37 -0700

    :P

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -11,11 +11,11 @@ unsigned long int hash_djb2(const unsigned char *str)
 	int c;
 
 	hash = 5381;
-	
+
 	while ((c = *str++))
 	{
 	hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
-	
+
 	return (hash);
 }
