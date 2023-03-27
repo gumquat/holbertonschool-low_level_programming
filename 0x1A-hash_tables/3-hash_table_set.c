e9b2b58c242395f2f0ebbf70945d7401b40dd231
gumquat <gumquat@gmail.com>
2023-03-27 11:16:57 -0700

    :P

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,7 +9,7 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = 0;
+	unsigned long int index;
 	hash_node_t *newNode;
 	hash_node_t *current;
 	
