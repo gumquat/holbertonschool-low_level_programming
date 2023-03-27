ad21f73921a791646d7bb4ca23174fa23c700593
gumquat <gumquat@gmail.com>
2023-03-27 11:45:01 -0700

    :P

---
+++ 0x1A-hash_tables/4-hash_tables_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ *hash_tables_get - retrieves a value associated with a key
+ *@ht: hash table
+ *@key: keeeeey
+ *Return: value from element on success, NULL on failure
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+		if (index >= ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	if (node == NULL)
+		return NULL;
+	else 
+		return (node->value);
+}
