9cfddce74bd84c1492865e15114fe5b707c0e98f
Evan Newman-Chock <gumquat@gmail.com>
2023-11-28 13:42:52 -0600

    last fucking push please im so tired

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -15,12 +15,12 @@ int linear_search(int *array, size_t size, int value)
 {
 int i;
 
-if(!array)
+if (!array)
 {
 return (-1);
 }
 
-for(i = 0; i < (int)size; i++)
+for (i = 0; i < (int)size; i++)
 {
 printf("Value checked array[%d] = [%d]\n", i, array[i]);
 if (array[i] == value)
