fe7e3a751429cca6a3c5d8a8dc8349f9d2dd024e
Evan Newman-Chock <gumquat@gmail.com>
2023-11-28 13:06:57 -0600

    update 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -15,7 +15,7 @@ int linear_search(int *array, size_t size, int value) {
   if (array == NULL) {
       return -1;
   }
-  for (i = 0; i < size; i++) {
+  for (size_t i = 0; i < size; i++) {
       printf("Current value: %d, Looking for: %d\n", array[i], value);
       if (array[i] == value) {
       }
