6318ba7a29e8b7dd7fd3326df22a33bd04359b8e
Evan Newman-Chock <gumquat@gmail.com>
2023-11-28 14:06:37 -0600

    whitespace

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -7,7 +7,7 @@
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in `array`
  * @value: the value to search for
- * Return: -1 if `value` is not present or if `array` is NULL 
+ * Return: -1 if `value` is not present or if `array` is NULL
  * else returns the index where the value was found
  */
 
