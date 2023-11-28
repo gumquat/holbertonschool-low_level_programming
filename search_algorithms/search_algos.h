b367e08fc08bca265fabd9e22dc11592052b5ff1
Evan Newman-Chock <gumquat@gmail.com>
2023-11-28 13:03:06 -0600

    adding library
    Signed-off-by: Evan Newman-Chock <gumquat@gmail.com>

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,39 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* standard libraries go here */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* structs & typedefs go here */
+/*
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node
+ *
+ * Desc.: single-linked list node structure
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+/* func PROTOTYPES */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
+/* HELPERS */
+skiplist_t *create_skiplist(int *array, size_t size);
+void free_skiplist(skiplist_t *list);
+void print_skiplist(const skiplist_t *list);
+int calc_skip(skiplist_t *list);
+
+#endif
