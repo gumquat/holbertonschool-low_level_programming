5a96c36e93398eaec85f458b10944c7c5203b946
Evan Newman-Chock <gumquat@gmail.com>
2023-11-28 13:27:34 -0600

    updates

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -13,18 +13,18 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+      int i;
 
-	if (!array)
-  {
-    return (-1);
-  }
+      if (!array)
+      {
+        return (-1);
+      }
 
-	for (i = 0; i < (int)size; i++)
-  {
-		printf("Value checked [%d] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-	}
-	return (-1);
+      for (i = 0; i < (int)size; i++)
+      {
+        printf("Value checked: [%d], Compared to: [%d]\n", i, array[i]);
+        if (array[i] == value)
+          return (i);
+      }
+      return (-1);
 }
